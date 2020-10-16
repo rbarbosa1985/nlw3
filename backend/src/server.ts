47dd117082f62fd7e1f1c34bf80008b36400ae6a
Roberto dos Santos Barbosa <roberto@MacBook-Pro-de-Roberto.local>
2020-10-15 22:02:34 -0300

    BackEnd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,37 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './model/Orphanage';
+import './database/connection';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.send('Hello World');
+app.post('/orphanages', async (request, response) => {
+
+      const{
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+      } = request.body;
+
+      const orphanagesRepository = getRepository(Orphanage);
+
+      const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends,
+      });
+
+      await orphanagesRepository.save(orphanage);
+
+      return response.send('Hello World');
 });
 
 app.listen(3333);
